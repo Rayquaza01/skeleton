a0f1cc46668da0d71a1b8ae98799ac02405494ec
Joe Jarvis <joe.jarvis109@gmail.com>
2018-10-12 23:47:03 -0400

    Split into functions, use os.path.join

--- skeleton.py
+++ skeleton.py
@@ -1,27 +1,38 @@
 #!/usr/bin/env python3
 import os
+import sys
 import shutil
 import argparse
-ap = argparse.ArgumentParser()
-ap.add_argument("skeltype", help="The file type of the skeleton")
-ap.add_argument("skelname", help="The name of the skeleton file", nargs="?")
-ap.add_argument("-n", "--name", help="The file name of the destination "
-                "file", nargs="?")
-opts = ap.parse_args()
-skelDir = os.path.dirname(__file__) + "/skeletons"
-curDir = os.getcwd()
-skelType = opts.skeltype
-if opts.skelname is not None:
-    skelName = opts.skelname
-else:
-    skelName = ""
-skelFile = ".".join([skelName, skelType])
-skelPath = "/".join([skelDir, skelFile])
-if os.path.isfile(skelPath):
-    shutil.copy(skelPath, curDir)
-else:
-    print("Could not find the skeleton file specified.")
-if opts.name is not None:
-    destPath = "/".join([curDir, skelFile])
-    destName = ".".join([opts.name, skelType])
-    os.rename(destPath, destName)
+
+
+def options():
+    ap = argparse.ArgumentParser()
+    ap.add_argument("skeltype", help="The file type of the skeleton")
+    ap.add_argument("skelname", help="The name of the skeleton file", nargs="?")
+    ap.add_argument("-n", "--name", help="The file name of the destination "
+                    "file", nargs="?")
+
+
+def main():
+    opts = options()
+    skelDir = os.path.join(os.path.dirname(sys.argv[0]) + "skeletons")
+    curDir = os.getcwd()
+    skelType = opts.skeltype
+    if opts.skelname is not None:
+        skelName = opts.skelname
+    else:
+        skelName = ""
+    skelFile = ".".join([skelName, skelType])
+    skelPath = os.path.join([skelDir, skelFile])
+    if os.path.isfile(skelPath):
+        shutil.copy(skelPath, curDir)
+    else:
+        print("Could not find the skeleton file specified.")
+    if opts.name is not None:
+        destPath = os.path.join([curDir, skelFile])
+        destName = ".".join([opts.name, skelType])
+        os.rename(destPath, destName)
+
+
+if __name__ == "main":
+    main()
